af7563f3380cc4074e88abab21578d3c6c7d8f99
Dlr <david.larouziere@gmail.com>
2021-12-13 08:59:34 +0100

    Sentinel options

--- src/client/client.options.ts
+++ src/client/client.options.ts
@@ -1,7 +1,10 @@
+import {SentinelOptions} from "./sentinel.options";
+
 export interface ClientOptions {
     url?: string;
     host?: string;
     port?: number;
     db?: number;
     password?: string;
+    sentinels? : SentinelOptions[];
 }
---
+++ src/client/sentinel.options.ts
@@ -0,0 +1,4 @@
+export class SentinelOptions {
+    port?: string;
+    host?: string;
+}
